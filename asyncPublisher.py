f1502383dedc61135395759019b5ef946dc572b7
Sandeep Chenna kumar <sandeepkchenna@gmail.com>
2021-03-24 12:24:25 +0530

    initial commit

---
+++ asyncPublisher.py
@@ -0,0 +1,60 @@
+import pika
+import logging
+
+log_format = ('%(levelname) -10s %(asctime)s %(name) -30s %(funcName) '
+              '-35s %(lineno) -5d: %(message)s')
+logger = logging.getLogger(__name__)
+rabbitmq_host = ""
+port = 5672
+
+
+class publish_engine:
+    def __init__(self):
+        self._number_of_messages = 20
+        self._channel = None
+        self._connection = None
+
+    def on_open(self, connection):
+        # Invoked when the connection is open
+        print("Connection open\n")
+        self._channel = self._connection.channel(on_open_callback=self.on_channel_open)
+
+    def on_declare(self, channel):
+        print("Received queue declared from RabbitMQ")
+        while self._number_of_messages > 0:
+            print(self._number_of_messages)
+
+            self._channel.basic_publish(exchange="",
+                                        routing_key="async_q",
+                                        body="H" + str(self._number_of_messages),
+                                        properties=pika.BasicProperties(content_type='text/plain',
+                                                                        delivery_mode=2))
+            self._number_of_messages -= 1
+        self._connection.close()
+
+    def on_channel_open(self, channel):
+        print("Receive 'Channel Created' from RabbitMQ")
+        argument_list = {"x-queue-master-locator": "random"}
+        self._channel.queue_declare(queue="async_q", callback=self.on_declare, durable=True, arguments=argument_list)
+
+    def on_close(self, connection, reply_code, ):
+        print(reply_code)
+        # print(reply_message)
+        print("CONNECTION CLOSED\n")
+
+    def run(self):
+        logging.basicConfig(level=logging.ERROR, format=log_format)
+        creds = pika.PlainCredentials("guest", "guest")
+        params = pika.ConnectionParameters(rabbitmq_host, port, "/", creds, socket_timeout=300)
+        self._connection = pika.SelectConnection(params, on_open_callback=self.on_open)
+        self._connection.add_on_close_callback(self.on_close)
+
+        try:
+            self._connection.ioloop.start()
+        except KeyboardInterrupt:
+            self._connection.close()
+
+
+if __name__ == "__main__":
+    engine = publish_engine()
+    engine.run()
